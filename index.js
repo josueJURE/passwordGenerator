fd17219306b224e26e298c80920f409d15152532
josueJURE <josue.jure@gmail.com>
2023-01-27 16:10:23 +0000

    create symbols array

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const displayPassword = document.getElementById('displayPassword');
 const lowerCaseLetters = [...Array(26)].map((_, i) => String.fromCharCode(i + 97));
 const upperCaseLetters = lowerCaseLetters.map(letter => letter.toUpperCase());
 const symbolsNumbers = [...Array(31)].map((_, i) => String.fromCharCode( i + 33));
-const numbers = symbolsNumbers.filter(filterOutNonDigits);
-console.log(numbers)
+const numbers = symbolsNumbers.filter(filterOutNonDigits); // digit zero is missing
+const symbols = symbolsNumbers.filter(filterOutDigits); // digit zero not filtered out
+console.log(symbols)
+
 
 
 
@@ -17,4 +19,9 @@ function generatePassword() {
 
 function filterOutNonDigits(val) {
     return Number(val)
+}
+
+function filterOutDigits(val) {
+    return !Number(val)
+
 }
