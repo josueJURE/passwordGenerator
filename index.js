9aa964d4f376dc30b9d3b7806b510249b3a45e5b
josueJURE <josue.jure@gmail.com>
2023-01-27 15:27:13 +0000

    create 'lower case array'

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const button = document.getElementById('button');
 const displayPassword = document.getElementById('displayPassword');
+const lowerCaseLetters = [...Array(26)].map((_, i) => String.fromCharCode(i + 97));
+console.log(lowerCaseLetters)
+
 
 
 button.addEventListener("click", generatePassword);
